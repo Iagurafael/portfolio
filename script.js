48c8dda90c12e6bbf641e2fa80ef2b176245185d
Iagurafael <iagorafael794@gmail.com>
2024-06-03 21:17:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.card').forEach(card => {
+    const video = card.querySelector('video');
+
+    card.addEventListener('mouseover', () => {
+        video.play();
+    });
+
+    card.addEventListener('mouseout', () => {
+        video.pause();
+    });
+});
